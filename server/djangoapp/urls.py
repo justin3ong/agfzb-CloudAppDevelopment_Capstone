49a0f05bd16320b180c7fbbd7d722cff76fc651e
justin3ong <jong2@77global.biz>
2022-08-06 14:36:46 +0000

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,20 +8,23 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-    #path(route='', view=views.index, name='index'),
-    # path for about view
-    path(route='', view=views.about, name='about'),
-    # path for contact us view
-    path(route='', view=views.contact, name='contact'),
-    # path for registration
     
+    # path for about view
+    path('about/', view=views.about, name='about'),
+    # path for contact us view
+    path('contact/', view=views.contact, name='contact'),
+    # path for registration
+    path('register/', view=views.registration_request, name='register'),
     # path for login
+    path('login', view=views.login_request, name='login'),
 
     # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
+    path('index/', view=views.get_dealerships, name='index'),
+
+    
 
     # path for add a review view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+#from .models import related models
+#from .restapis import related methods
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -15,7 +15,6 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
 
 # Create an `about` view to render a static about page
 def about(request):
@@ -30,14 +29,41 @@ def contact(request):
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
+def login_request(request):
+    context = {}
+    # Handles POST request
+    if request.method == "POST":
+        # Get username and password from request.POST dictionary
+        username = request.POST['username']
+        password = request.POST['psw']
+        # Try to check if provide credential can be authenticated
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # If user is valid, call login method to login current user
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            # If not, return to login page again
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        return render(request, 'djangoapp/index.html', context)
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
+def logout_request(request):
+    # Get the user object based on session id in request
+    print("Log out the user `{}`".format(request.user.username))
+    # Logout user in the request
+    logout(request)
+    # Redirect user back to course list view
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
+def registration_request(request):
+    return render(request,'djangoapp/registration.html')
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
