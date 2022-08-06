b66f9cd2ed47d7a00ced5f1a7d662b5fc14e44dd
justin3ong <jong2@77global.biz>
2022-08-06 15:26:50 +0000

    changes made from theia lab environment

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,3 +1,29 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@justin3ong 
+dennislamcv1
+/
+IBMFullStackSoftware
+Public
+Code
+Issues
+Pull requests
+Actions
+Security
+Insights
+IBMFullStackSoftware/Full Stack Cloud Development Capstone Project/server/djangoapp/models.py /
+@dennislamcv1
+dennislamcv1 Add files via upload
+Latest commit 963f90a on Jun 23, 2021
+ History
+ 1 contributor
+98 lines (77 sloc)  2.73 KB
+
 from django.db import models
 from django.utils.timezone import now
 
@@ -9,6 +35,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+    desc = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -20,8 +53,72 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+class CarModel(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+    id = models.CharField(max_length=2)
+    type = models.CharField(max_length=10)
+    year = models.DateField(null=True)
+    carmakes = models.ManyToManyField(CarMake)
+
+    def __str__(self):
+        return self.name
+
+
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
+
+
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
+class DealerReview:
+
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        # Dealership
+        self.dealership = dealership
+        # Dealer name
+        self.name = name
+        # Dealer purchase
+        self.purchase = purchase
+        # Dealer review
+        self.review = review
+        # purchase_date
+        self.purchase_date = purchase_date
+        # car_make
+        self.car_make = car_make
+        # car_model
+        self.car_model = car_model
+        # car_year
+        self.car_year = car_year
+        # sentiment
+        self.sentiment = sentiment
+        # id
+        self.id = id
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -22,10 +22,11 @@ urlpatterns = [
     path(route='logout/', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
-    path('index/', view=views.get_dealerships, name='index'),
-
+    path(route='', view=views.get_dealerships, name='index'),
+    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
     
 
     # path for add a review view
+    path(route='dealer/<int:dealer_id>/', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
